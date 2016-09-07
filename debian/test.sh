97c67cbb7993eb08d80dc0f75529d83e1a8d054d
Simon McVittie <smcv@debian.org>
2016-09-07 21:04:12 +0100

    Terminate stray gpg-agent processes when running tests

---
+++ debian/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+e=0
+dh_auto_test || e=$?
+
+echo "Killing gpg-agent processes:"
+pgrep --list-full --full "gpg-agent --homedir /var/tmp/test-flatpak-.*" >&2 || :
+pgrep --list-full --full "gpg-agent --homedir /var/tmp/flatpak-test-.*" >&2 || :
+pkill --full "gpg-agent --homedir /var/tmp/test-flatpak-.*" >&2 || :
+pkill --full "gpg-agent --homedir /var/tmp/flatpak-test-.*" >&2 || :
