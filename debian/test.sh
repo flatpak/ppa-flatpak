d2ea1daed9c55902a70d12edae2a26be1b52232a
Simon McVittie <smcv@debian.org>
2017-07-03 09:30:22 +0100

    debian/test.sh: Do not ignore build-time tests' exit status

--- debian/test.sh
+++ debian/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 e=0
 dh_auto_test || e=$?
@@ -8,3 +9,4 @@ pgrep --list-full --full "gpg-agent --homedir /var/tmp/test-flatpak-.*" >&2 || :
 pgrep --list-full --full "gpg-agent --homedir /var/tmp/flatpak-test-.*" >&2 || :
 pkill --full "gpg-agent --homedir /var/tmp/test-flatpak-.*" >&2 || :
 pkill --full "gpg-agent --homedir /var/tmp/flatpak-test-.*" >&2 || :
+exit "$e"
