42eb34214e9fa0e1cdfbcf4cf6a60b8cda922306
Simon McVittie <smcv@debian.org>
2018-06-07 19:50:08 +0100

    Output test logs in the build log, even on success

--- debian/test.sh
+++ debian/test.sh
@@ -4,6 +4,11 @@ set -e
 e=0
 dh_auto_test || e=$?
 
+find . -name '*.log' \
+-not -name config.log \
+-not -name test-suite.log \
+-print0 | xargs -0 tail -v -c1M
+
 echo "Killing gpg-agent processes:"
 pgrep --list-full --full "gpg-agent --homedir /var/tmp/test-flatpak-.*" >&2 || :
 pgrep --list-full --full "gpg-agent --homedir /var/tmp/flatpak-test-.*" >&2 || :
